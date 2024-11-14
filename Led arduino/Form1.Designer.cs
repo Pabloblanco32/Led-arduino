99579f194a31c5b85154e247bf113fb8eaacf595
pablo <pablo@DANIEL>
2024-11-14 12:05:11 -0600

    Agregar archivos de proyecto.

---
+++ Led arduino/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Led_arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Image = global::Led_arduino.Properties.Resources.Captura_de_pantalla_2024_11_14_113552;
+            this.button2.Location = new System.Drawing.Point(318, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 305);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Encender";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Led_arduino.Properties.Resources.Captura_de_pantalla_2024_11_14_113321;
+            this.button1.Location = new System.Drawing.Point(77, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 305);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Apagar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(690, 329);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
